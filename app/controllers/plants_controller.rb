13dc16dbe0e98aa7bc84d1c656bc7f28189f73d9
Jake Manley <jbmanley45@gmail.com>
2022-12-07 13:42:29 -0800

    commit tree 342cbbeb776b585a6907cfb9e655e36413dd3420

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,10 +17,26 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  
+  def update
+    plant = Plant.find_by(id: params[:id])
+    plant.update(plant_params)
+    render json: plant, status: :accepted
+  end
+
+  def destroy
+    plant = Plant.find(params[:id]) 
+    plant.destroy
+  end
 
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+  def plant_params 
+    params.permit(:name, :price, :is_in_stock, :image)
+  end
+
+
 end
